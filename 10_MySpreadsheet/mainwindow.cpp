a31d1ae44b91551de9c9c427b3774179e7f794d6
fisyamaloy <fisyuklol@gmail.com>
2023-06-20 14:37:40 +0300

    Open&Save file realisation

--- 10_MySpreadsheet/mainwindow.cpp
+++ 10_MySpreadsheet/mainwindow.cpp
@@ -1,6 +1,10 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 #include <QIcon>
+#include <QLabel>
+#include <QDebug>
+#include <QMessageBox>
+#include <QFileDialog>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -20,6 +24,7 @@ MainWindow::MainWindow(QWidget *parent)
         alphabet.push_back(QString(QChar(i)));
     }
     ui->tableWidget->setHorizontalHeaderLabels(alphabet);
+
     ui->tableWidget->addAction(ui->actioncut);
     ui->tableWidget->addAction(ui->actionCopy);
     ui->tableWidget->addAction(ui->actionPaste);
@@ -38,6 +43,16 @@ MainWindow::MainWindow(QWidget *parent)
 
     ui->actionShow_grid->setChecked(ui->tableWidget->showGrid());
 
+    locationLabel = new QLabel(" W999 ");
+    locationLabel->setAlignment(Qt::AlignHCenter);
+    locationLabel->setMinimumSize(locationLabel->sizeHint());
+
+    formulaLabel = new QLabel;
+    formulaLabel->setIndent(3);
+    statusBar()->addWidget(locationLabel, 0);
+    statusBar()->addWidget(formulaLabel, 1);
+
+    setCurrentFile("");
     setWindowIcon(QIcon(":/Icons/Spreadsheet.jpg"));
 }
 
@@ -65,3 +80,186 @@ void MainWindow::on_actionShow_grid_toggled(bool arg1)
 {
     ui->tableWidget->setShowGrid(arg1);
 }
+
+void MainWindow::on_tableWidget_currentCellChanged(int currentRow, int currentColumn, int previousRow, int previousColumn)
+{
+    updateStatusBar(currentRow, currentColumn);
+}
+
+void MainWindow::updateStatusBar(const int row, const int column)
+{
+    if (row == -1 || column == -1)
+        return;
+
+    auto horHeaderItem = ui->tableWidget->horizontalHeaderItem(column);
+    const QString COLUMN_HEADER = (horHeaderItem != nullptr) ? horHeaderItem->text() : "";
+
+    locationLabel->setText(COLUMN_HEADER + QString::number(row + 1));
+}
+
+void MainWindow::on_actionNew_triggered()
+{
+    if (okToContinue())
+    {
+        clearTableWidget();
+        setCurrentFile("");
+    }
+}
+
+bool MainWindow::okToContinue()
+{
+    if (isWindowModified())
+    {
+        int res = QMessageBox::warning(this, tr("Spreadsheet"), tr("The document has been modified. \n Do you want to save your changes?"),
+                                       QMessageBox::Yes | QMessageBox::No | QMessageBox::Cancel);
+        if (res == QMessageBox::Yes)
+            return true;
+        else if (res == QMessageBox::Cancel)
+            return false;
+
+    }
+    return true;
+}
+
+bool MainWindow::save()
+{
+    if (currentFile.isEmpty())
+        return saveAs();
+    else
+        return saveFile(currentFile);
+}
+
+bool MainWindow::saveAs()
+{
+    const QString FILE_NAME = QFileDialog::getSaveFileName(this, tr("Save Spreadsheet"), ".", tr("Spreadsheet files (*.sp)"));
+    if (FILE_NAME.isEmpty())
+        return false;
+
+    return saveFile(FILE_NAME);
+}
+
+bool MainWindow::writeFile(const QString& fileName)
+{
+    QFile file(fileName);
+    if (!file.open(QIODevice::WriteOnly | QIODevice::Text))
+        return false;
+
+    QTextStream stream(&file);
+
+    const int ROW_AMOUNT = ui->tableWidget->rowCount();
+    const int COLUMN_AMOUNT = ui->tableWidget->columnCount();
+    for (int row = 0; row < ROW_AMOUNT; ++row) {
+        for (int col = 0; col < COLUMN_AMOUNT; ++col) {
+            QTableWidgetItem* item = ui->tableWidget->item(row, col);
+            if (item) {
+                QString value = item->text();
+                stream << value << "\t";  // Разделитель значений
+            }
+        }
+        stream << "\n";  // Переход на новую строку
+    }
+
+    // Закрытие файла
+    file.close();
+    return true;
+}
+
+bool MainWindow::saveFile(const QString& fileName)
+{
+    if (!writeFile(fileName))
+    {
+        statusBar()->showMessage(tr("Saving canceled"), 2000);
+        return false;
+    }
+
+    setCurrentFile(fileName);
+    statusBar()->showMessage(tr("File saved"), 2000);
+    return true;
+}
+
+void MainWindow::setCurrentFile(const QString& filePath)
+{
+    currentFile = filePath;
+    setWindowModified(false);
+
+    QString showName = "Untitled";
+    if (!currentFile.isEmpty())
+    {
+        showName = strippedName(currentFile);
+        recentFiles.removeAll(currentFile);
+        recentFiles.prepend(currentFile);
+        updateRecentFilesActions();
+    }
+    setWindowTitle(tr("%1[*] - %2").arg(showName).arg(tr("Spreadsheet")));
+}
+
+QString MainWindow::strippedName(const QString& filePath)
+{
+    QFileInfo file(filePath);
+    return file.fileName();
+}
+
+void MainWindow::updateRecentFilesActions()
+{
+
+}
+
+void MainWindow::clearTableWidget()
+{
+    for (int i = 0; i < ui->tableWidget->rowCount(); ++i)
+    {
+        for (int j = 0; j < ui->tableWidget->columnCount(); ++j)
+        {
+            auto item = ui->tableWidget->takeItem(i, j);
+            if (item)
+            {
+                delete item;
+            }
+        }
+    }
+}
+
+void MainWindow::on_actionOpen_triggered()
+{
+    if (okToContinue())
+    {
+        QString fileName = QFileDialog::getOpenFileName(this, tr("Open Spreadsheet"), ".", tr("Spreadsheet files (*.sp)"));
+        if (!fileName.isEmpty())
+            loadFile(fileName);
+    }
+}
+
+bool MainWindow::loadFile(const QString& fileName)
+{
+    QFile file(fileName);
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+        return false;
+    }
+
+    QTextStream stream(&file);
+    int row = 0;
+    while (!stream.atEnd()) {
+        QString line = stream.readLine();
+        QStringList values = line.split("\t");
+
+        int column = 0;
+        for (const QString& value : values) {
+            QTableWidgetItem* item = new QTableWidgetItem(value);
+            ui->tableWidget->setItem(row, column, item);
+            ++column;
+        }
+
+        ++row;
+    }
+
+    file.close();
+    return true;
+}
+
+
+void MainWindow::on_actionSave_triggered()
+{
+    save();
+}
+
--- 10_MySpreadsheet/mainwindow.h
+++ 10_MySpreadsheet/mainwindow.h
@@ -3,7 +3,9 @@
 
 #include <QMainWindow>
 
+
 //class QAction;
+class QLabel;
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -26,9 +28,44 @@ private slots:
 
     void on_actionShow_grid_toggled(bool arg1);
 
+    void on_tableWidget_currentCellChanged(int currentRow, int currentColumn, int previousRow, int previousColumn);
+
+    void on_actionNew_triggered();
+
+    void on_actionOpen_triggered();
+
+    void on_actionSave_triggered();
+
 private:
+    bool okToContinue();
+
+    void updateStatusBar(const int row, const int column);
+
+    void clearTableWidget();
+
+    void setCurrentFile(const QString& filePath);
+
+    bool save();
+    bool saveAs();
+    bool saveFile(const QString& fileName);
+
+    bool writeFile(const QString& fileName);
+
+    bool loadFile(const QString& fileName);
+
+    QString strippedName(const QString& filePath);
+
+    void updateRecentFilesActions();
+
     Ui::MainWindow *ui;
+
     static constexpr int RECENT_FILES_AMOUNT = 5;
     QAction* recentFilesActions[RECENT_FILES_AMOUNT];
+    QStringList recentFiles;
+
+    QLabel* locationLabel;
+    QLabel* formulaLabel;
+
+    QString currentFile;
 };
 #endif // MAINWINDOW_H
